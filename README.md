53afa41599e6ffcdedaef8190a5875a18c4583b2
st1990pg <st1990pg@gmail.com>
2020-10-28 17:46:07 +0100

    debugging

--- README.md
+++
@@ -1,1 +0,0 @@
-# treci-cas
---
+++ test.js
@@ -0,0 +1,10 @@
+export default class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  returnUser() {
+    console.log(this);
+  }
+}
