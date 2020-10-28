42519210d0ddcedb37f0da777353065c09c82484
st1990pg <st1990pg@gmail.com>
2020-10-28 17:35:56 +0100

    Push test file na main

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('');
+export default class User {
+  constructor(testName, testage) {
+    this.testName = name;
+    this.testage = age;
+  }
+
+  returnUser() {
+    console.log('ovo je test' + this.testName + this.testage);
+  }
+}
